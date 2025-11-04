32e8939c698c4745f94391335b7b3a3994ff6814
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2025-11-04 12:38:48 -0800

    SDK

--- gosset_sdk/__init__.py
+++ gosset_sdk/__init__.py
@@ -5,13 +5,7 @@ Gosset SDK - Python SDK for Gosset Drug Database
 __version__ = "0.1.0"
 
 from .auth import get_oauth_token
-<<<<<<< HEAD
 from .client import GossetClient, GossetAPIError
 
 __all__ = ["get_oauth_token", "GossetClient", "GossetAPIError", "__version__"]
-=======
-from .client import GossetClient
-
-__all__ = ["get_oauth_token", "GossetClient", "__version__"]
->>>>>>> b164c9c (Save)
 
